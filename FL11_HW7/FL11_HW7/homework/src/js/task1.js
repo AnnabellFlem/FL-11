5762fa29d6077cbbbb9da06759f2b94f8ac16acf
Annabell <Ts.Nyutka@gmail.com>
2019-07-09 21:41:05 +0300

    add full hw7

--- FL11_HW7/FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/FL11_HW7/homework/src/js/task1.js
@@ -1,49 +1,47 @@
-const email = prompt("Enter your email", '');
+const email = prompt('Enter your email', '');
 
-if(!email) {
-    alert("Canceled.");
+if (!email) {
+    alert('Canceled.');
 } else if (email.length < 6) {
     alert("I don't know any emails having name length less than 6 symbols");
-}
-
-const pswd = "";
-if (email === "user@gmail.com" || email === "admin@gmail.com") {
-    pswd = prompt("Enter your password", '');
 } else {
-    alert("I don’t know you");
-}
-
-if(!pswd) {
-    alert("Canceled.");
-} else if (email === "user@gmail.com" &&  pswd === "UserPass" || 
-            email === "admin@gmail.com" &&  pswd === "AdminPass") {
-    alert("Welcome.");
-} else {
-    alert("Wrong password");
-}
-
-const isChangePassword = confirm("Do you want to change your password?");
-
-const oldPassword = "";
-const newPassword = "";
-if (isChangePassword) {
-    oldPassword = prompt("Enter your old password", '');
-    if (email === "user@gmail.com" &&  oldPassword === "UserPass" || 
-            email === "admin@gmail.com" &&  oldPassword === "AdminPass") {
-        newPassword = prompt("Enter your new password", '');
+    let pswd = '';
+    if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+        pswd = prompt('Enter your password', '');
+        if (!pswd) {
+            alert('Canceled.');
+        } else if (email === 'user@gmail.com' && pswd === 'UserPass' ||
+            email === 'admin@gmail.com' && pswd === 'AdminPass') {
+            alert('Welcome.');
+            const isChangePassword = confirm('Do you want to change your password?');
+            let oldPassword = '';
+            let newPassword = '';
+            if (isChangePassword) {
+                oldPassword = prompt('Enter your old password', '');
+                if (email === 'user@gmail.com' && oldPassword === 'UserPass' ||
+                    email === 'admin@gmail.com' && oldPassword === 'AdminPass') {
+                    newPassword = prompt('Enter your new password', '');
+                    let repeatPassword = '';
+                    if (newPassword.length < 5) {
+                        alert('It’s too short password. Sorry.');
+                    } else {
+                        repeatPassword = prompt('Enter your new password again', '');
+                        if (repeatPassword === newPassword) {
+                            alert('You have successfully changed your password.');
+                        } else {
+                            alert('You wrote the wrong password.');
+                        }
+                    }
+                } else {
+                    alert('Wrong password');
+                }
+            } else {
+                alert('You have failed the change.');
+            }
+        } else {
+            alert('Wrong password');
+        }
     } else {
-        alert("You have failed the change.");
-    }
-}
-
-const repeatPassword = "";
-if (newPassword.length < 5) {
-    alert("It’s too short password. Sorry.");
-} else {
-    repeatPassword = prompt("Enter your new password again", '');
-    if (repeatPassword === newPassword) {
-        alert("You wrote the wrong password.");
-    }   else {
-        alert("You have successfully changed your password.");
+        alert('I don’t know you');
     }
 }
--- FL11_HW7/FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/FL11_HW7/homework/src/js/task2.js
@@ -1,59 +1,64 @@
-let randLim = 1;//
+const minRandLim = 8;
+let randLim = minRandLim;
 let prize;
 let currPrize;
+const maxAttempts = 3;
 let numAttempts;
 let isWin = false;
 let randomNumber;
 let totalPrize = 0;
 let isGame;
+let isError = false;
 
 do {
     prize = 100;
     currPrize = prize;
-    numAttempts = 1;//
-    
-    isGame = confirm("Do you want to play a game?");
+    numAttempts = maxAttempts;
+
+    isGame = confirm('Do you want to play a game?');
 
     if (!isGame) {
-        alert("You did not become a billionaire, but can.");
+        alert('You did not become a billionaire, but can.');
     } else {
-        let userNumber = "";
+        let userNumber;
+        randomNumber = Math.floor(Math.random() * (randLim + 1));
 
         do {
-            randomNumber = Math.floor(Math.random() * randLim);
-            userNumber = prompt("Enter a number of pocket on which the ball could land", "");
-            alert(`${userNumber}:${randomNumber}:${userNumber == randomNumber}`);
-            //console.log(randomNumber, userNumber, userNumber === randomNumber, userNumber == randomNumber);
-            if (userNumber == randomNumber) {
-                let isContinue = confirm(`Congratulation, you won! Your prize is: ${currPrize}$. Do you want to continue?`);
+            userNumber = prompt(
+                `Choose o roulette pocket number from 0 to ${randLim}
+Attempts left: ${numAttempts}
+Total prize: ${totalPrize}
+Possible prize on current attempt: ${prize}`, '');
+            userNumber = parseInt(userNumber, 10);
+            isError = isNaN(userNumber);
+            if (isError) {
+                alert('Incorrect number. Try again.');
+            }
+            if (userNumber === randomNumber) {
+                let isContinue = confirm(
+                    `Congratulation, you won! Your prize is: ${currPrize}$. Do you want to continue?`);
                 totalPrize += currPrize;
                 if (isContinue) {
                     randLim += 4;
-                    prize *= 4;
+                    prize *= 2;
                     currPrize = prize;
-                    numAttempts = 2;//
+                    numAttempts = maxAttempts + 1;
+                    randomNumber = Math.floor(Math.random() * (randLim + 1));
                 } else {
                     numAttempts = 1;
                 }
                 isWin = true;
-            } 
-            currPrize -= currPrize / 2;
+            } else {
+                currPrize /= 2;
+                isWin = false;
+                if (numAttempts === 1) {
+                    totalPrize = 0;
+                    randLim = minRandLim;
+                }
+            }
         } while (--numAttempts);
 
-        alert(`Thank you for your participation. Your prize is: ${ isWin ? totalPrize : 0 }$`); 
+        alert(`Thank you for your participation. Your prize is: ${isWin ? totalPrize : 0}$`);
+
     }
 } while (isGame);
-
-// for (let i = numAttempts; i < 0; i--) {
-//     if (userNumber === randomNumber) {
-//         alert(`Congratulation, you won! Your prize is: ${prize}$. Do you want to continue?`);
-//     } else {
-//         prize -= prize / 2;
-//         i--;
-//         userNumber = prompt("Enter a number of pocket on which the ball could land", "");
-//     }
-//     if (numAttempts === 0) {
-//         alert(`Thank you for your participation. Your prize is: ${prize}$`);
-//     }
-// }
-//}
